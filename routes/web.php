85922e521495a1cf5dc837e1c08d442b972e307a
hoainam05-suy <namnhph49013@gmail.com>
2025-05-22 13:49:51 +0700

    fact:category,order,rooms,user

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,28 @@ Route::get('/admin', function () {
 });
 
 Route::get('/admin/rooms', function () {
-    return view('admin.rooms.index');
+    return view('admin.rooms.rooms');
 });
+Route::get('/admin/category/add', function () {
+    return view('admin.category.addcategory');
+});
+Route::get('/admin/category', function () {
+    return view('admin.category.listcategory');
+});
+
+Route::get('/admin/user', function () {
+    return view('admin.user.user');
+});
+Route::get('/admin/user/add', function () {
+    return view('admin.user.adduser');
+});
+
+Route::get('/admin/order', function () {
+    return view('admin.order.order');
+});
+
+Route::get('/admin/order/detail', function () {
+    return view('admin.order.orderdetail');
+});
+
+
