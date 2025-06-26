d43a81321fbf7d519e04dac115c447c4c4ebb3fc
VuxDiHoc <nguyenvu14112005@gmail.com>
2025-06-26 15:37:41 +0700

    fix tim kiem ben user

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -40,51 +40,60 @@ class HomeController extends Controller
     }
 
     public function indexRoom(Request $request)
-    {
-        $checkIn = $request->input('check_in');
-        $checkOut = $request->input('check_out');
-        $adults = (int) $request->input('adults', 0);
-        $children = (int) $request->input('children', 0);
-        $childAges = $request->input('child_ages', []);
+{
+    $checkIn = $request->input('check_in');
+    $checkOut = $request->input('check_out');
+    $adults = (int) $request->input('adults', 0);
+    $children = (int) $request->input('children', 0);
+    $childAges = $request->input('child_ages', []);
 
-        $isSearching = $checkIn || $checkOut || ($adults > 0 || $children > 0);
+    $isSearching = $checkIn || $checkOut || ($adults > 0 || $children > 0);
 
-        $roomTypesQuery = RoomType::with(['rooms' => function ($query) use ($isSearching, $adults, $childAges) {
-            $query->with(['images_room', 'roomType'])->where('status', 1);
+    // ✅ Hardcoded số giường tương ứng với mỗi loại phòng
+    $bedConfig = [
+        1 => 1, // room_type_id = 1 => Single
+        2 => 2, // Twin
+        3 => 1, // Double
+        4 => 3, // Triple
+    ];
 
-            if ($isSearching) {
-                $totalPeople = $adults;
-                $under6Count = 0;
-                $under12Count = 0;
+    $totalBedsNeeded = $adults;
+    $under12 = 0;
 
-                foreach ($childAges as $age) {
-                    $age = (int) $age;
-                    if ($age < 6) {
-                        $under6Count++;
-                    } elseif ($age < 12) {
-                        $under12Count++;
-                    } else {
-                        $totalPeople++;
-                    }
-                }
-                if ($under6Count > 1) {
-                    $totalPeople += ($under6Count - 1);
-                }
-                $query->where('max_people', '>=', $totalPeople)->orderBy('price', 'asc');
-            } else {
-                $query->orderBy('created_at', 'desc');
-            }
-        }]);
-
-        $roomTypes = $roomTypesQuery->get();
-
-        $allRooms = $roomTypes->pluck('rooms')->flatten();
-        $amenityIds = $allRooms->pluck('amenities')->filter()->flatten()->unique()->toArray();
-        $allAmenities = Amenitie::whereIn('id', $amenityIds)->get()->keyBy('id');
-
-
-        return view('client.room.index', compact('roomTypes', 'allAmenities', 'checkIn', 'checkOut', 'adults', 'children', 'childAges'));
+    foreach ($childAges as $age) {
+        $age = (int) $age;
+        if ($age >= 12) {
+            $totalBedsNeeded += 1;
+        } else {
+            $under12 += 1;
+        }
     }
+
+    $totalBedsNeeded += ceil($under12 / 2);
+
+    $roomTypesQuery = RoomType::with(['rooms' => function ($query) use ($isSearching, $bedConfig, $totalBedsNeeded) {
+        $query->with(['images_room', 'roomType'])->where('status', 1);
+
+        if ($isSearching) {
+            $allowedRoomTypeIds = array_keys(array_filter($bedConfig, function ($beds) use ($totalBedsNeeded) {
+                return $beds >= $totalBedsNeeded;
+            }));
+
+            $query->whereIn('room_type_id', $allowedRoomTypeIds)->orderBy('price', 'asc');
+        } else {
+            $query->orderBy('created_at', 'desc');
+        }
+    }]);
+
+    $roomTypes = $roomTypesQuery->get();
+
+    $allRooms = $roomTypes->pluck('rooms')->flatten();
+    $amenityIds = $allRooms->pluck('amenities')->filter()->flatten()->unique()->toArray();
+    $allAmenities = Amenitie::whereIn('id', $amenityIds)->get()->keyBy('id');
+
+    return view('client.room.index', compact('roomTypes', 'allAmenities', 'checkIn', 'checkOut', 'adults', 'children', 'childAges'));
+}
+
 
     public function show($id)
     {
