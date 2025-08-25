a8069b50d3af9bb3651ebbb98a34918a2466496e
ZuuYeuDoi <165405299+ZuuYeuDoi@users.noreply.github.com>
2025-08-25 21:47:04 +0700

    sửa luồng tìm kiếm

--- resources/views/client/tours/index.blade.php
+++ resources/views/client/tours/index.blade.php
@@ -210,15 +210,6 @@
             </form>
         @endif
 
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
 
         <hr class="title-divider">
 
